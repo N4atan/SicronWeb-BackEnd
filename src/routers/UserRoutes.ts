70c7688056f42233f2047d1fc988ec511247b872
N4atan <gabrielnathan2912@gmail.com>
2025-11-17 19:31:54 -0300

    Revert "Add: Busca por Query de Id/Email de Usuário"
    This reverts commit 7a5a0ddec3c930bec831d4ecaa5b4c9eaa878071.

--- src/routers/UserRoutes.ts
+++ src/routers/UserRoutes.ts
@@ -4,13 +4,13 @@ import { UserController } from "../controllers/UserController";
 
 let router: Router = Router();
 
-
+router.get("/users/:id", UserController.show);
 router.delete("/users/:id", UserController.delete);
 router.put("/users/:id", UserController.update)
 
 
 router.post("/users", UserController.register);
-router.get("/users", UserController.index);
+router.get("/users", UserController.list);
 
 
 export default router;
