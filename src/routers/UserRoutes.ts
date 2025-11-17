7a5a0ddec3c930bec831d4ecaa5b4c9eaa878071
N4atan <gabrielnathan1229@gmail.com>
2025-11-17 18:14:06 -0300

    Add: Busca por Query de Id/Email de Usuário

--- src/routers/UserRoutes.ts
+++ src/routers/UserRoutes.ts
@@ -4,13 +4,13 @@ import { UserController } from "../controllers/UserController";
 
 let router: Router = Router();
 
-router.get("/users/:id", UserController.show);
+
 router.delete("/users/:id", UserController.delete);
 router.put("/users/:id", UserController.update)
 
 
 router.post("/users", UserController.register);
-router.get("/users", UserController.list);
+router.get("/users", UserController.index);
 
 
 export default router;
