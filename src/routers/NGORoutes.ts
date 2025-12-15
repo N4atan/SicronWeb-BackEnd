f151e7019e18e05cfb0129052eb917a15c74ecf7
N4atan <gabrielnathan2912@gmail.com>
2025-12-15 12:20:04 -0300

    Fix: Middleware Login

--- src/routers/NGORoutes.ts
+++ src/routers/NGORoutes.ts
@@ -1,23 +1,17 @@
 import { Router } from "express";
 import { NGOController } from "../controllers/NGOController";
-
 import { loginChecker           } from "../middlewares/loginChecker";
 import { loginRequire           } from "../middlewares/loginRequire";
 import { loginManagerPrivillege } from "../middlewares/loginManagerPrivillege";
-
 let router: Router = Router();
-
 router.use(loginChecker);
-
+// Rotas Públicas (ou semi-públicas)
 router.get("/", NGOController.query);
-
 router.use(loginRequire);
-
+// Rotas Autenticadas
 router.post("/", NGOController.register);
-
-router.use(loginManagerPrivillege);
-
-router.patch("/:uuid", NGOController.update);
-router.delete("/:uuid", NGOController.delete);
-
+// REMOVIDO: router.use(loginManagerPrivillege); <--- ISSO CAUSAVA O ERRO
+// ADICIONADO: Middleware injetado diretamente na rota, após o path '/:uuid'
+router.patch("/:uuid", loginManagerPrivillege, NGOController.update);
+router.delete("/:uuid", loginManagerPrivillege, NGOController.delete);
 export default router;
