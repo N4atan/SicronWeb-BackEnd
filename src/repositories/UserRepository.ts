9ed6d53ddf618fa94851cccc14d7d1e3a92dcb99
N4atan <gabrielnathan2912@gmail.com>
2025-12-17 10:46:47 -0300

    melhorias na exibição do perfil

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -4,43 +4,39 @@ import { AppDataSource } from "../config/data-source";
 
 import { User } from '../entities/User'
 
-export class UserRepository
-{
+export class UserRepository {
     private repository: Repository<User> = AppDataSource.getRepository(User);
 
-    public async createAndSave( data: Partial<User> ): Promise<User>
-    {
+    public async createAndSave(data: Partial<User>): Promise<User> {
         const user = this.repository.create(data);
         return this.repository.save(user);
     }
 
-    public async findAll(options?: FindManyOptions<User>): Promise<User[]>
-    {
+    public async findAll(options?: FindManyOptions<User>): Promise<User[]> {
         return this.repository.find(options);
     }
 
-    public async findById(id: number): Promise<User | null>
-    {
+    public async findById(id: number): Promise<User | null> {
         return this.repository.findOneBy({ id });
     }
-    
-    public async findByUUID(uuid: string): Promise<User | null>
-    {
+
+    public async findOne(options: any): Promise<User | null> {
+        return this.repository.findOne(options);
+    }
+
+    public async findByUUID(uuid: string): Promise<User | null> {
         return this.repository.findOneBy({ uuid });
     }
 
-    public async findByEmail(email: string): Promise<User | null>
-    {
+    public async findByEmail(email: string): Promise<User | null> {
         return this.repository.findOneBy({ email })
     }
 
-    public async save(user: User): Promise<User>
-    {
+    public async save(user: User): Promise<User> {
         return this.repository.save(user);
     }
 
-    public async remove(user: User): Promise<User>
-    {
+    public async remove(user: User): Promise<User> {
         return this.repository.remove(user);
     }
 }
